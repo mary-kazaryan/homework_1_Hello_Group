7f62902999a2baed47131a024b904cb2e1c3da2b
Mary Kazaryan <82213157+mary-kazaryan@users.noreply.github.com>
2021-04-09 23:36:32 +0300

    Added a new class Hello_Group
    Added a new class Hello_Group to print "Hello" "TeachMeSkills group" on separate lines.

---
+++ src/com/company/Hello_Group.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Hello_Group {
+
+    public static void main (String[] args) {
+	System.out.println("Hello");
+	System.out.println ("TeachMeSkills group");
+    }
+}
